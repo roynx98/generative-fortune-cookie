e60bf494f887ce25f11028691593b0afc9c3ad5b
roynx98 <roynx98@gmail.com>
2025-06-29 16:46:50 -0600

    feat(main): Set server_name

--- src/main.py
+++ src/main.py
@@ -67,8 +67,7 @@ def main():
             elem_classes="center-text",
         )
 
-    demo.launch()
-
+    demo.launch(server_name="0.0.0.0", server_port=7860)
 
 if __name__ == "__main__":
     main()
